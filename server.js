959a634a2d4c487393756d8b8aa4d4065b993608
cserbancea <cserbancea@gmail.com>
2018-03-25 01:40:44 +0200

    heroku updates

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
-
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -68,6 +69,6 @@ app.get('/trend', (req, res) => {
   res.send('Bla Bla Bla');
 });
 
-app.listen(3000, () => {
-  console.log('server is up on port 3000');
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
 });
